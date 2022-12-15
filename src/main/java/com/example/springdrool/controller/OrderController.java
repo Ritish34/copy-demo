0a0d05b392ea4407e4dbe0e5f432101a433e4dea
Dhruvi <dhruvip.inexture@gmail.com>
2022-12-15 11:45:38 +0530

    added greet

--- src/main/java/com/example/springdrool/controller/OrderController.java
+++ src/main/java/com/example/springdrool/controller/OrderController.java
@@ -2,8 +2,8 @@ package com.example.springdrool.controller;
 
 import com.example.springdrool.model.Order;
 import com.example.springdrool.service.DiscountService;
-import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +17,11 @@ public class OrderController {
     @PostMapping("/getDiscount")
     public int getDiscount(@RequestBody Order order){
         return discountService.getDiscount(order);
+    }
+
+    @GetMapping("/")
+    public String greet(){
+        System.out.println("Hellooooooo");
+        return "<h2>Helloooo</h2>";
     }
 }
