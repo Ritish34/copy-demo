915adea27f7db57353f9a220778aa66ff4dcf5c5
Dhruvi <dhruvip.inexture@gmail.com>
2022-12-16 12:08:05 +0530

    configuration change

--- src/main/java/com/example/springdrool/SpringDroolApplication.java
+++ src/main/java/com/example/springdrool/SpringDroolApplication.java
@@ -2,18 +2,12 @@ package com.example.springdrool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDroolApplication extends SpringBootServletInitializer {
+public class SpringDroolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDroolApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringApplication.class);
-	}
 }
